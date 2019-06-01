b26f287ec2a519e3a7c0a0405f360d8dd245fa48
sebasdrk17 <alexis7_hernandez14@hotmail.com>
2019-06-01 17:29:56 -0500

    Ya se puede leer los valores del tiempo del RTC

--- main.c
+++ main.c
@@ -2,27 +2,42 @@
 #FUSES NOWDT,NOFCMEN,NOIESO,PUT,NOBROWNOUT,NOPBADEN,NOLPT1OSC,NOLVP,NOXINST
 #use delay(crystal=16000000)
 #use rs232(baud=9600,parity=N,xmit=PIN_C6,rcv=PIN_C7,bits=8)
-#use i2c(MASTER,FAST=100000,SDA=PIN_C4,SCL=PIN_C3)
+#use i2c(MASTER,FAST,SDA=PIN_C4,SCL=PIN_C3)
 #define SLAVE_WRITE_ADDR 0xD0
 #define SLAVE_READ_ADDR 0xD1
 #use standard_io(c)
 
+int BCDaBIN(BYTE bcd);
+void rtc_get_time(BYTE& hr, BYTE& min, BYTE& sec);
+
+int seg,min,hr;
+
 void main(){
     int i=0;
-    while(TRUE){
-        i2c_start();
-        i2c_write(SLAVE_READ_ADDR);
-        int segundos=0,minutos=0,hora=0;
-        segundos=i2c_read(1);
-        minutos=i2c_read(1);
-        hora=i2c_read(0);
-        printf("SEG:%d\n\rMIN:%d\n\rHORA:%d\n\r",segundos,minutos,hora);
-        printf("algo");
-        getch();
-        i++;
-        if(i>5){
-            FALSE;
-        }
+    for(i=0;i<63;i++){
+        rtc_get_time(hr,min,seg);
+        printf("%d\n\r",seg);
+        delay_ms(100);
     }
-    i2c_stop();
+    rtc_get_time(hr,min,seg);
+    printf("funcion%d\n\r",seg);
+}
+
+int BCDaBIN(BYTE bcd){
+    int varia;
+    varia=bcd;
+    varia>>=1;
+    varia &= 0x78;
+    return (varia + (varia>>2) + (bcd & 0x0f));
+}
+void rtc_get_time(BYTE& hr, BYTE& min, BYTE& sec) {
+    i2c_start(); //Escritura
+    i2c_write(0xD0); //Codigo de escritura
+    i2c_write(0x00); //Puntero de la primera dirección
+    i2c_start(); //Lectura
+    i2c_write(0xD1); //Codigo de lectura
+    sec = BCDaBIN(i2c_read()&0x7f); //Lectura de los 7 bit de los segundos
+    min = BCDaBIN(i2c_read()&0x7f); //Lectura de los 7 bit de los minutos
+    hr = BCDaBIN(i2c_read(0)&0x3f); //Lectura de los 6 bit de las horas
+    i2c_stop(); //Finaliza comunicación
 }
